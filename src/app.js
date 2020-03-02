ead2be630d353eb3f95024be1ea365d8afc49d85
Rupali <rupalirajput@gmail.com>
2020-03-02 16:09:37 -0600

    first commit

---
+++ src/app.js
@@ -0,0 +1,62 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const { NODE_ENV } = require("./config");
+const ArticlesService = require("./articles-service");
+
+const app = express();
+
+app.use(morgan(NODE_ENV === "production" ? "tiny" : "common"));
+app.use(cors());
+app.use(helmet());
+
+app.get("/articles", (req, res, next) => {
+  const knexInstance = req.app.get("db");
+  ArticlesService.getAllArticles(knexInstance)
+    .then(articles => {
+      const test = articles.map(article => ({
+        id: article.id,
+        title: article.title,
+        style: article.style,
+        content: article.content,
+        date_published: article.date_published //new Date
+      }));
+      console.log(test);
+      res.json(test);
+    })
+    .catch(next);
+});
+
+app.get("/articles/:article_id", (req, res, next) => {
+  const knexInstance = req.app.get("db");
+  ArticlesService.getById(knexInstance, req.params.article_id)
+    .then(article => {
+      res.json({
+        id: article.id,
+        title: article.title,
+        style: article.style,
+        content: article.content,
+        date_published: new Date(article.date_published)
+      });
+    })
+    .catch(next);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+app.use(function errorHandler(error, req, res, next) {
+  let response;
+  if (NODE_ENV === "production") {
+    response = { error: "Server error" };
+  } else {
+    console.error(error);
+    response = { message: error.message, error };
+  }
+  res.status(500).json(response);
+});
+
+module.exports = app;
